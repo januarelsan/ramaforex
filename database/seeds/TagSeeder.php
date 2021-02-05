e213c53ff451b2b4e00be222f2ca0071d6b2c805
januarelsan <januarelsan@gmail.com>
2021-02-05 22:47:57 +0700

    Add Tag Seeder

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tags')->insert([
+            'value' => "kaya",                    
+        ]);
+
+        DB::table('tags')->insert([
+            'value' => "uang",                    
+        ]);
+    }
+}

5651fb1229c61f33ebd4315d29339a022c6b2a6b
januarelsan <januarelsan@gmail.com>
2021-02-05 22:52:04 +0700

    Update Index View
    - Update Header
    - Update Sub Header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -58,47 +58,11 @@
                     </div>
                     <div class="uk-navbar-right uk-width-expand uk-flex uk-flex-right">
                         <ul class="uk-navbar-nav uk-visible@m">
-                            <li><a href="index-2.html">Home<i class="fas fa-chevron-down"></i></a>
-                                <div class="uk-navbar-dropdown">
-                                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li><a href="homepage2.html">Homepage 2</a></li>
-                                        <li><a href="homepage3.html">Homepage 3</a></li>
-                                        <li><a href="homepage4.html">Homepage 4</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a href="markets.html">Markets</a></li>
-                            <li><a href="#">Company<i class="fas fa-chevron-down"></i></a>
-                                <div class="uk-navbar-dropdown">
-                                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li><a href="about.html">About</a></li>
-                                        <li><a href="blog-list.html">Blog</a></li>
-                                        <li><a href="careers.html">Careers</a></li>
-                                        <li><a href="contact.html">Contact</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a href="education.html">Education</a></li>
-                            <li><a href="#">Resources<i class="fas fa-chevron-down"></i></a>
-                                <div class="uk-navbar-dropdown uk-navbar-dropdown-width-2">
-                                    <div class="uk-navbar-dropdown-grid uk-child-width-1-2" data-uk-grid>
-                                        <div>
-                                            <ul class="uk-nav uk-navbar-dropdown-nav">
-                                                <li><a href="https://getuikit.com/docs/">Documentation<i class="fas fa-external-link-square-alt fa-sm"></i></a></li>
-                                                <li><a href="help-center.html">Help Center</a></li>
-                                                <li><a href="customers.html">Customers</a></li>
-                                                <li><a href="roadmap.html">Roadmap</a></li>
-                                                <li><a href="legal-docs.html">Legal Docs<i class="fas fa-gavel fa-sm"></i></a></li>
-                                            </ul>
-                                        </div>
-                                        <div>
-                                            <ul class="uk-nav uk-navbar-dropdown-nav">
-                                                <li><a class="uk-disabled" href="#">Adipiscing elit sed do eiusmod incididunt ut labore dolore magna lorem ipsum sit dolor amet consectetur</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
+                            <li><a href="#">Home<i class="fas fa-chevron"></i></a></li>
+                            <li><a href="#">Testimony<i class="fas fa-chevron"></i></a></li>
+                            <li><a href="#">About Us<i class="fas fa-chevron"></i></a></li>
+                            <li><a href="#">Blog<i class="fas fa-chevron"></i></a></li>
+                            
                         </ul>
                         <div class="uk-navbar-item uk-visible@m in-optional-nav">
                             <a href="signin.html" class="uk-button uk-button-text"><i class="fas fa-user-circle uk-margin-small-right"></i>Log in</a>
@@ -114,10 +78,9 @@
                         <div class="uk-card uk-card-secondary uk-card-small uk-card-body uk-border-rounded">
                             <div class="uk-grid uk-text-small" data-uk-grid>
                                 <div class="uk-width-3-4@m uk-visible@m">
-                                    <p>Trading involves substantial risk and may result in the loss of your invested/greater that your invested capital, respectively.</p>
+                                    <p>Hubungi Kami Segera Agar Anda Cepat Kaya!</p>
                                 </div>
-                                <div class="uk-width-expand@m uk-text-center uk-text-right@m">
-                                    <a class="uk-margin-right" href="#"><i class="fas fa-comment-alt uk-margin-small-right"></i>Live Chat</a>
+                                <div class="uk-width-expand@m uk-text-center uk-text-right@m">                                    
                                     <a href="#"><i class="fas fa-phone-alt uk-margin-small-right uk-margin-small-left"></i>1-800-123-4567</a>
                                 </div>
                             </div>
@@ -141,48 +104,15 @@
                             <div class="uk-grid" data-uk-grid>
                                 <div class="uk-width-3-5@m">
                                     <div class="uk-overlay">
-                                        <h1>Trade Shares and Forex with Financial Thinking</h1>
-                                        <p class="uk-text-lead uk-visible@m">Access 40,000+ instruments – across asset classes – to trade, hedge and invest from a single account.</p>
+                                        <h1>Trading Dengan Robot Lebih Aman</h1>
+                                        <p class="uk-text-lead uk-visible@m">Biarkan Robot Bekerja Untuk Anda!</p>
                                         <a href="#" class="uk-button uk-button-primary uk-border-rounded uk-visible@m"><i class="fas fa-scroll uk-margin-small-right"></i>Discover it now</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </li>
-                    <li>
-                        <div class="uk-position-cover">
-                            <img src="{{ asset('wave/img/in-lazy.gif')}}" data-src="{{ asset('wave/img/in-wave-background-2.png')}}" alt="slideshow-image" data-uk-cover width="1920" height="700" data-uk-img>
-                        </div>
-                        <span></span>
-                        <div class="uk-container">
-                            <div class="uk-grid" data-uk-grid>
-                                <div class="uk-width-3-5@m">
-                                    <div class="uk-overlay">
-                                        <h1>Multi-regulated Global Forex and Shares Broker</h1>
-                                        <p class="uk-text-lead uk-visible@m">A trusted destination for traders worldwide, Authorised by FCA, ASIC &amp; FSCA with multi-lingual support 24/5.</p>
-                                        <a href="#" class="uk-button uk-button-primary uk-border-rounded uk-visible@m"><i class="fas fa-scroll uk-margin-small-right"></i>Discover it now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="uk-position-cover">
-                            <img src="{{ asset('wave/img/in-lazy.gif')}}" data-src="{{ asset('wave/img/in-wave-background-3.jpg')}}" alt="slideshow-image" data-uk-cover width="1920" height="700" data-uk-img>
-                        </div>
-                        <span></span>
-                        <div class="uk-container">
-                            <div class="uk-grid" data-uk-grid>
-                                <div class="uk-width-3-5@m">
-                                    <div class="uk-overlay">
-                                        <h1>Award-winning Products and Trading platforms</h1>
-                                        <p class="uk-text-lead uk-visible@m">Tap into the world's markets and explore endless trading opportunities with tight spreads and no commission.</p>
-                                        <a href="#" class="uk-button uk-button-primary uk-border-rounded uk-visible@m"><i class="fas fa-scroll uk-margin-small-right"></i>Discover it now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                    
                 </ul>
                 <div class="uk-container uk-light">
                     <ul class="uk-slideshow-nav uk-dotnav uk-position-bottom-center"></ul>

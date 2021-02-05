cb96fdb0af63e0a36d49ff1693ebf1f6ad3f8d56
januarelsan <januarelsan@gmail.com>
2021-02-06 01:53:29 +0700

    Add Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\ArticleController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');

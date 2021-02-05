6c3dd9bd52a3bef6d06f5ce78ff174b5ef26e094
januarelsan <januarelsan@gmail.com>
2021-02-06 02:18:07 +0700

    Add Role When Create User

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\UserRole;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,11 +65,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        UserRole::create([
+            'user_id' => $user['id'],
+            'role_id' => 4,            
+        ]);
+
+        return $user;
     }
 
     public function showRegistrationForm() 

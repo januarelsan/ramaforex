a191bf320afeb9da591a95dc41e3e238d946ddfb
januarelsan <januarelsan@gmail.com>
2021-02-06 01:50:58 +0700

    Add Function
    - Override showRegistrationForm

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,4 +70,11 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    public function showRegistrationForm() 
+    {
+        return view('myauth.register');
+    }
+
+    
 }

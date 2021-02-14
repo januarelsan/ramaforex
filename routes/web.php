e04161238f8cd81166837f1a5025edb0e878fb0f
januarelsan <januarelsan@gmail.com>
2021-02-14 21:45:27 +0700

    Add Routes
    - About Us
    - Legal

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
+Route::get('/legal', function () {
+    return view('legal');
+})->name('legal');
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+})->name('aboutus');
+
 Route::get('/blog/article/detail/{id}', [ArticleController::class, 'show'])->name('blog.detail.show');
 Route::get('/blog/article/list', [ArticleController::class, 'list'])->name('blog.list');
 Route::get('/home', 'HomeController@index')->name('home');
